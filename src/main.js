38e9daf14dea745e6ed78d2a4a40b65e82ff97e9
Anton <a.chertok@akatech.com.ua>
2023-12-12 13:55:36 +0000

    refactor

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import './js/homeCategories';
+// import './js/homeCategories';
 import './js/favorites';
-import './js/themeBackground';
-import './js/activePageHeader';
-import './js/mobile-modal';
+// import './js/themeBackground';
+// import './js/activePageHeader';
+// import './js/mobile-modal';

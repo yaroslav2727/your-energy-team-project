c02d420b741e093621dd6dedda26d674a5ab39e4
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-12 17:36:54 +0000

    Deploying to gh-pages from @ yaroslav2727/your-energy-team-project@cdbdc1e57d2079d1672ec264d1d2786d2d39f0b6 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import"./assets/activePageHeader-8c7cb6b7.js";import{a as u}from"./assets/vendor-a61d8330.js";const p="https://your-energy.b.goit.study/api",l=12,g=10,c=u.create({baseURL:p,params:{}}),m=async(e,s=1)=>(await c.get("/filters",{params:{filter:e,page:s,limit:l}})).data,x=async(e=1)=>(await c.get("/filters",{params:{filter:"Body parts",page:e,limit:l}})).data,v=async({bodypart:e,muscles:s,equipment:a,search:t},n=1)=>{const r=t&&!e&&!s&&!a?"":t;return(await c.get("/exercises",{params:{bodypart:e||"",muscles:s||"",equipment:a||"",keyword:r||"",page:n,limit:g}})).data};function d(e){return e.map(({imgURL:s,filter:a,name:t})=>`<li class="card" data-body-home='${t}'>
+import"./assets/activePageHeader-353345dc.js";import{a as p}from"./assets/vendor-a61d8330.js";const g="https://your-energy.b.goit.study/api",d=12,m=10,o=p.create({baseURL:g,params:{}}),x=async(e,s=1)=>(await o.get("/filters",{params:{filter:e,page:s,limit:d}})).data,v=async(e=1)=>(await o.get("/filters",{params:{filter:"Body parts",page:e,limit:d}})).data,f=async({bodypart:e,muscles:s,equipment:a,search:t},r=1)=>{const n=t&&!e&&!s&&!a?"":t;return(await o.get("/exercises",{params:{bodypart:e||"",muscles:s||"",equipment:a||"",keyword:n||"",page:r,limit:m}})).data};function u(e){return e.map(({imgURL:s,filter:a,name:t})=>`<li class="card" data-body-exercise='${t}'>
                <button type="button" class="cat-img"
                  style=" background: linear-gradient(0deg, rgba(17, 17, 17, 0.50) 0%, rgba(17, 17, 17, 0.50) 100%),
                  url('${s}') ;
@@ -11,7 +11,7 @@ import"./assets/activePageHeader-8c7cb6b7.js";import{a as u}from"./assets/vendor
    <p class="name-category">${t}</p>
       <p class="name-exercises">${a}</p>
   </div>
-    </li>`).join("")}function h(e){return e.map(({rating:s,name:a,burnedCalories:t,bodyPart:n,target:r,_id:o})=>`<li class="exercises-item container" data-exercise-id="${o}">
+    </li>`).join("")}function y(e){return e.map(({rating:s,name:a,burnedCalories:t,bodyPart:r,target:n,_id:l})=>`<li class="exercises-item container" data-exercise-id="${l}">
 		<div class="exercises-item-header">
 			<div class="exercises-rating-wrapper">
 				<p class="exercises-text">WORKOUT</p>
@@ -44,13 +44,13 @@ import"./assets/activePageHeader-8c7cb6b7.js";import{a as u}from"./assets/vendor
 				</li>
 				<li class="exercises-data-item">
 					<span class="exercises-data-info">Body part:</span>
-					${n}
+					${r}
 				</li>
 				<li class="exercises-data-item">
 					<span class="exercises-data-info">Target:</span>
-					${r}
+					${n}
 				</li>
 			</ul>
 		</div>
-	</li>`).join("")}let y=1;const i=document.querySelector(".cards"),b=document.querySelector(".filter-list");b.addEventListener("click",f);i.addEventListener("click",L);x().then(e=>{const s=e.results;console.log(s),i.innerHTML=d(s)}).catch(e=>{console.error(e)});function f(e){if(e.target.nodeName!=="BUTTON")return;const s=e.target.dataset.name;console.log(s),m(s,y).then(a=>{const t=a.results;console.log(t),i.innerHTML=d(t)}).catch(a=>{console.error(a)})}function L(e){let s=e.target.closest(".card").dataset.bodyHome;console.log(s),v({bodypart:s,page:1}).then(t=>{const n=t.results;console.log(n),i.innerHTML=h(n)}).catch(t=>{console.error(t)})}function w(){const e=document.querySelector(".js-menu-container"),s=document.querySelector(".js-open-menu"),a=document.querySelector(".js-close-menu"),t=document.body,n=()=>{const r=s.getAttribute("aria-expanded")==="true"||!1;s.setAttribute("aria-expanded",!r),e.classList.toggle("is-open"),e.classList.contains("is-open")?t.style.overflow="hidden":t.style.overflow="visible"};s.addEventListener("click",n),a.addEventListener("click",n),window.matchMedia("(min-width: 768px)").addEventListener("change",r=>{r.matches&&(e.classList.remove("is-open"),s.setAttribute("aria-expanded",!1))})}w();
+	</li>`).join("")}let b=1,i="bodypart";const c=document.querySelector(".cards"),h=document.querySelector(".filter-list");h.addEventListener("click",L);c.addEventListener("click",w);v().then(e=>{const s=e.results;console.log(s),c.innerHTML=u(s)}).catch(e=>{console.error(e)});function L(e){if(e.target.nodeName!=="BUTTON")return;const s=e.target.dataset.name;console.log(s),document.querySelectorAll(".filter-btn").forEach(t=>t.classList.remove("active")),e.target.classList.add("active"),x(s,b).then(t=>{const r=t.results,{filter:n}=r[0];i=n.toLowerCase(),c.innerHTML=u(r)}).catch(t=>{console.error(t)})}function w(e){const s=e.target.closest(".card").dataset.bodyExercise;console.log("exercise",s),console.log("category",i);const a={[i]:s,page:1};f(a).then(t=>{const r=t.results;c.innerHTML=y(r)}).catch(t=>{console.error(t)})}function E(){const e=document.querySelector(".js-menu-container"),s=document.querySelector(".js-open-menu"),a=document.querySelector(".js-close-menu"),t=document.body,r=()=>{const n=s.getAttribute("aria-expanded")==="true"||!1;s.setAttribute("aria-expanded",!n),e.classList.toggle("is-open"),e.classList.contains("is-open")?t.style.overflow="hidden":t.style.overflow="visible"};s.addEventListener("click",r),a.addEventListener("click",r),window.matchMedia("(min-width: 768px)").addEventListener("change",n=>{n.matches&&(e.classList.remove("is-open"),s.setAttribute("aria-expanded",!1))})}E();
 //# sourceMappingURL=commonHelpers2.js.map

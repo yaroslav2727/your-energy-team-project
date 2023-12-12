959cb7cb34bf174500be7266628c7a18457ea837
SerAnatoliy <anatoliy.serduikov@gmail.com>
2023-12-12 14:43:39 +0100

    working

--- src/js/modalWindow.js
+++ src/js/modalWindow.js
@@ -3,22 +3,27 @@ import { getExerciseById } from "./api/api";
 console.log(getExerciseById('64f389465ae26083f39b1817'));
 
 
-const openModalButton = document.querySelector(".exercises-btn");
+const cardsList = document.querySelector(".js_cards");
 const modalExcercise= document.querySelector(".modal_window_container")
 const closeModalButton = document.querySelector(".close_modal_button")
 
-const openModal = function () {
+const openModal = function (event) {
+    if (cardsList.closest(".exercises-btn")) {
+        console.log(event.target);
     modalExcercise.classList.remove("is_hidden");
+    }
+    else return
+    
 }
+
 const closeModal = function () {
     modalExcercise.classList.add("is_hidden")
   }
-openModalButton.addEventListener('click', openModal);
+cardsList.addEventListener('click', openModal);
 closeModalButton.addEventListener('click', closeModal);
 
-openModalButton.addEventListener('click', getExerciseById);
 
-function markupModal(data) {
-    return data
-    .map(({}))
-}
+// function markupModal(data) {
+//     return data
+//     .map(({}))
+// }

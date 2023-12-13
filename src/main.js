f22653ea8e9e32577bd75ee3613c9128c9cf2e38
vinvas94 <vintoniv1111@gmail.com>
2023-12-13 23:42:18 +0100

    addstaticbutton

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
-import "./js/homeCategories";
-import "./js/themeBackground";
-import "./js/activePageHeader";
-import "./js/mobileModal";
-import "./js/rating";
-import "./js/modalWindow";
-
+import './js/homeCategories';
+import './js/themeBackground';
+import './js/mobileModal';
+import './js/rating';
+import './js/modalWindow';

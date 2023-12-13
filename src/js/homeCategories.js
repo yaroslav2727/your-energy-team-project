16c45eb39d6a5aaaeaab93ed0b5882cedc532e69
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-13 19:29:56 +0200

    fix

--- src/js/homeCategories.js
+++ src/js/homeCategories.js
@@ -1,19 +1,20 @@
-import { getMusclesList, getFilteredList, getExercises } from './api/api';
-import { markupCategories } from './markupCategories';
-import { markupExercises } from './markupExercises';
-import { onSearchExercise } from "./searchExercise"
-
+import { getMusclesList, getFilteredList, getExercises } from "./api/api";
+import { markupCategories } from "./markupCategories";
+import { markupExercises } from "./markupExercises";
+import { loader } from "./utils/loader";
 
 let currentPage = 1;
-let category = 'muscles';
+let category = "muscles";
 
-const items = document.querySelector('.cards');
-const filter = document.querySelector('.filter-list');
+const items = document.querySelector(".cards");
+const filter = document.querySelector(".filter-list");
 
-filter.addEventListener('click', handlerClickCategory);
-items.addEventListener('click', handlerClickExercises);
+filter.addEventListener("click", handlerClickCategory);
+items.addEventListener("click", handlerClickExercises);
 
 // стартовий список
+loader.create();
+
 getMusclesList()
   .then(response => {
     const data = response.results;
@@ -21,19 +22,24 @@ getMusclesList()
   })
   .catch(err => {
     console.error(err);
+  })
+  .finally(() => {
+    loader.destroy();
   });
 
 // фільтр по категоріям
 function handlerClickCategory(e) {
-  if (e.target.nodeName !== 'BUTTON') {
+  if (e.target.nodeName !== "BUTTON") {
     return;
   }
 
   const categoryName = e.target.dataset.name;
 
-  const filterBtn = document.querySelectorAll('.filter-btn');
-  filterBtn.forEach(btn => btn.classList.remove('active'));
-  e.target.classList.add('active');
+  const filterBtn = document.querySelectorAll(".filter-btn");
+  filterBtn.forEach(btn => btn.classList.remove("active"));
+  e.target.classList.add("active");
+
+  loader.create();
 
   getFilteredList(categoryName, currentPage)
     .then(response => {
@@ -45,30 +51,32 @@ function handlerClickCategory(e) {
     })
     .catch(err => {
       console.error(err);
+    })
+    .finally(() => {
+      loader.destroy();
     });
 }
 
 // вивід списка вибраних вправ
 function handlerClickExercises(e) {
+  const exercise = e.target.closest(".card-exercises").dataset.bodyExercise;
 
-  const exercise = e.target.closest('.card-exercises').dataset.bodyExercise;
-  // console.log('exercise', exercise)
-  // console.log('category', category)
   const data = {
     [category]: exercise,
     page: 1,
   };
 
+  loader.create();
+
   getExercises(data)
     .then(response => {
       const data = response.results;
-      // console.log(filteredData)
       items.innerHTML = markupExercises(data);
-      onSearchExercise(data)
-
     })
     .catch(err => {
       console.error(err);
+    })
+    .finally(() => {
+      loader.destroy();
     });
 }
-
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
-import './js/homeCategories';
-import './js/themeBackground';
-import './js/activePageHeader';
-import './js/mobileModal';
-import "./js/cutDataCardsInfo";
-import "./js/searchExercise";
-
+import "./js/homeCategories";
+import "./js/themeBackground";
+import "./js/activePageHeader";
+import "./js/mobileModal";
+import "./js/rating";
+import "./js/modalWindow";

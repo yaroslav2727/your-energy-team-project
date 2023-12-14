24b1a87a0087aae431eb7d37b87fade05f550508
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-14 10:14:26 +0200

    add rating.js

--- src/js/rating.js
+++ src/js/rating.js
@@ -1,61 +1,60 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-import { patchRating } from '../js/api/api'
+import { patchRating } from "../js/api/api";
 
-const modalReff = document.querySelector("[data-modal]")
-const reatingTitleReff = document.querySelector('p[data-raiting]')
-const btnCloseReff = document.querySelector('.btn_close')
-const formReff = document.querySelector('.rating_form')
+const modalReff = document.querySelector("[data-modal]");
+const reatingTitleReff = document.querySelector("p[data-raiting]");
+const btnCloseReff = document.querySelector(".btn_close");
+const formReff = document.querySelector(".rating_form");
 const ratingStarsReff = [...document.getElementsByClassName("btn_star")];
 
-let raiting = 0
-let formItems = {}
-let id = '64f389465ae26083f39b17a2'
+let raiting = 0;
+let formItems = {};
+let id = "64f389465ae26083f39b17a2";
 
-btnCloseReff.addEventListener('click', (evt) => {
-  evt.preventDefault()
+btnCloseReff.addEventListener("click", evt => {
+  evt.preventDefault();
   // modalReff.classList.add('is-hidden')
-})
+});
 
-
-formReff.addEventListener('submit', (evt) => {
-  evt.preventDefault()
+formReff.addEventListener("submit", evt => {
+  evt.preventDefault();
   // const { email, review } = evt.currentTarget.elements
   // formItems = {
   //   email: email.value,
   //   review: review.value,
   //   rait: raiting
   // }
-  const formData = new FormData(evt.currentTarget)
+  const formData = new FormData(evt.currentTarget);
   formData.forEach((value, key) => {
-    formItems[key] = value.trim()
-  })
-  formItems.rate = raiting
+    formItems[key] = value.trim();
+  });
+  formItems.rate = raiting;
 
   if (formItems.rate < 1) {
     iziToast.show({
-      title: 'The rate must be at least 1',
-      color: 'red',
-      position: 'topCenter',
-      message: ``
+      title: "The rate must be at least 1",
+      color: "red",
+      position: "topCenter",
+      message: ``,
     });
-    return
+    return;
   }
 
-  const response = patchRating(id, formItems)
+  const response = patchRating(id, formItems);
   if (response) {
     iziToast.show({
-      title: 'The rating has been successfully added',
-      color: 'green',
-      position: 'topCenter',
-      message: ``
+      title: "The rating has been successfully added",
+      color: "green",
+      position: "topCenter",
+      message: ``,
     });
 
-    formReff.reset()
-    raiting = 0
-    modalReff.classList.add('is-hidden')
+    formReff.reset();
+    raiting = 0;
+    modalReff.classList.add("is-hidden");
   }
-})
+});
 
 function executeRating(stars) {
   const starClassActive = "btn_star-active";
@@ -63,7 +62,7 @@ function executeRating(stars) {
   const starsLength = stars.length;
   let i;
 
-  stars.map((star) => {
+  stars.map(star => {
     star.onclick = () => {
       i = stars.indexOf(star);
 
@@ -73,15 +72,15 @@ function executeRating(stars) {
         for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
       }
 
-      const rait = stars.filter((star) => {
+      const rait = stars.filter(star => {
         if (star.className === starClassActive) {
-          return star
+          return star;
         }
-      })
-      raiting = rait.length
-      reatingTitleReff.textContent = `${raiting}.0`
+      });
+      raiting = rait.length;
+      reatingTitleReff.textContent = `${raiting}.0`;
     };
-  })
+  });
 }
 
 executeRating(ratingStarsReff);
@@ -99,7 +98,7 @@ executeRating(ratingStarsReff);
 
   function toggleModal() {
     if (modalReff.classList.contains("is-hidden") === true) {
-      document.addEventListener("keydown", onEscapeKeyPress)
+      document.addEventListener("keydown", onEscapeKeyPress);
     }
 
     refs.modal.classList.toggle("is-hidden");
@@ -112,14 +111,9 @@ executeRating(ratingStarsReff);
   }
 
   function onEscapeKeyPress(evt) {
-    if (evt.key === 'Escape') {
+    if (evt.key === "Escape") {
       refs.modal.classList.add("is-hidden");
-      document.removeEventListener("keydown", onEscapeKeyPress)
+      document.removeEventListener("keydown", onEscapeKeyPress);
     }
   }
-
 })();
-
-
-
-

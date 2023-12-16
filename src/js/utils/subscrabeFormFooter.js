63d065e3b1f619484932cc6f8a3d7162ba440968
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-16 09:46:32 +0200

    subscribe-from-added-to-main

--- src/js/utils/subscrabeFormFooter.js
+++ src/js/utils/subscrabeFormFooter.js
@@ -1,22 +1,22 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { createSubscription } from './api';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import { createSubscription } from "../api/api";
 
-const emailInput = document.querySelector('.subscribe-input');
-const submitButton = document.querySelector('.submit-btn');
+const emailInput = document.querySelector(".subscribe-input");
+const submitButton = document.querySelector(".submit-btn");
 
 function isEmailUnique(email) {
-  const storedEmails = JSON.parse(localStorage.getItem('subscribedEmails')) || [];
+  const storedEmails = JSON.parse(localStorage.getItem("subscribedEmails")) || [];
   return !storedEmails.includes(email);
 }
 
 function saveEmailToLocalStorage(email) {
-  let storedEmails = JSON.parse(localStorage.getItem('subscribedEmails')) || [];
+  let storedEmails = JSON.parse(localStorage.getItem("subscribedEmails")) || [];
   storedEmails.push(email);
-  localStorage.setItem('subscribedEmails', JSON.stringify(storedEmails));
+  localStorage.setItem("subscribedEmails", JSON.stringify(storedEmails));
 }
 
-submitButton.addEventListener('click', async function (event) {
+submitButton.addEventListener("click", async function (event) {
   event.preventDefault();
 
   const email = emailInput.value.trim();
@@ -24,47 +24,47 @@ submitButton.addEventListener('click', async function (event) {
   if (validateEmail(email)) {
     if (isEmailUnique(email)) {
       try {
-        const response = await createSubscription({ email }); 
+        const response = await createSubscription({ email });
 
         if (response) {
           saveEmailToLocalStorage(email);
           iziToast.show({
-            title: 'Email subscribed successfully!',
-            color: 'green',
-            position: 'topCenter',
+            title: "Email subscribed successfully!",
+            color: "green",
+            position: "topCenter",
             message: ``,
           });
-          emailInput.value = ''; 
+          emailInput.value = "";
         } else {
           iziToast.show({
-            title: 'Failed to subscribe!',
-            color: 'red',
-            position: 'topCenter',
+            title: "Failed to subscribe!",
+            color: "red",
+            position: "topCenter",
             message: ``,
           });
         }
       } catch (error) {
-        console.error('Error:', error);
+        console.error("Error:", error);
         iziToast.show({
-          title: 'An error occurred!',
-          color: 'red',
-          position: 'topCenter',
+          title: "An error occurred!",
+          color: "red",
+          position: "topCenter",
           message: ``,
         });
       }
     } else {
       iziToast.show({
-        title: 'Email already subscribed!',
-        color: 'red',
-        position: 'topCenter',
+        title: "Email already subscribed!",
+        color: "red",
+        position: "topCenter",
         message: ``,
       });
     }
   } else {
     iziToast.show({
-      title: 'Invalid email format!',
-      color: 'red',
-      position: 'topCenter',
+      title: "Invalid email format!",
+      color: "red",
+      position: "topCenter",
       message: ``,
     });
   }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import './js/homeCategories';
-import './js/themeBackground';
-import './js/mobileModal';
-import './js/rating';
-import './js/modalWindow';
-import './js/quote';
+import "./js/homeCategories";
+import "./js/themeBackground";
+import "./js/mobileModal";
+import "./js/rating";
+import "./js/modalWindow";
+import "./js/quote";
+import "./js/utils/subscrabeFormFooter";

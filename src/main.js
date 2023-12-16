c300129f25e925402a32f30307788f6e4bf11ba8
salandr <ansaliy200013@gmail.com>
2023-12-16 15:08:38 +0000

    test

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import "./js/homeCategories";
-import "./js/themeBackground";
-import "./js/mobileModal";
-import "./js/rating";
-import "./js/modalWindow";
-import "./js/quote";
-import "./js/utils/subscrabeFormFooter";
+import './js/homeCategories';
+import './js/themeBackground';
+import './js/mobileModal';
+import './js/rating';
+import './js/modalWindow';
+import './js/quote';
+import './js/utils/subscrabeFormFooter';
+import './js/utils/button-up';

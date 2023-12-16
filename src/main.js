cb222e348db781b294b5c29d5b5cefada9484269
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-16 17:42:37 +0200

    path fixes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './js/homeCategories';
-import './js/themeBackground';
-import './js/mobileModal';
-import './js/rating';
-import './js/modalWindow';
-import './js/quote';
-import './js/utils/subscrabeFormFooter';
-import './js/utils/button-up';
+import "./js/homeCategories";
+import "./js/themeBackground";
+import "./js/mobileModal";
+import "./js/rating";
+import "./js/modalWindow";
+import "./js/quote";
+import "./js/subscrabeFormFooter";
+import "./js/button-up";
 // import "./js/privacy-policy";

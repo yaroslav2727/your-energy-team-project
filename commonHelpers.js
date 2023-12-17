8a504584b5982c455578e27751e72a77129c6b54
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-17 16:56:13 +0000

    Deploying to gh-pages from @ yaroslav2727/your-energy-team-project@9e49e47f21b33424ccf820cbda9a0a7040524bc9 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import"./assets/mobileModal-ae43c0fa.js";import{i as c,F as S,a as b,P as T,b as u,S as w}from"./assets/quote-97c20a96.js";import"./assets/vendor-c2f48a40.js";const r=(t,e)=>{if(e<=3)throw new Error("Trim length must not be less than 4");return t.length<=e-3?t:t.slice(0,e-3)+"..."};function P({_id:t,burnedCalories:e,time:a,bodyPart:s,target:i,name:h}){return`    <li class="favorites__item js-common-card-item" data-exercise-id="${t}">
+import"./assets/mobileModal-47c7b88c.js";import{i as c,F as S,a as b,P as T,b as u,S as w}from"./assets/quote-198c6c4b.js";import"./assets/vendor-c2f48a40.js";const r=(t,e)=>{if(e<=3)throw new Error("Trim length must not be less than 4");return t.length<=e-3?t:t.slice(0,e-3)+"..."};function P({_id:t,burnedCalories:e,time:a,bodyPart:s,target:i,name:h}){return`    <li class="favorites__item js-common-card-item" data-exercise-id="${t}">
                 <div class="favorites__card" href="">
                   <div class="sport-card">
                     <div class="sport-card__header">

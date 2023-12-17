fd1e700770bffcd9660f6adc9bba6a6f8e336353
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-17 10:13:53 +0200

    privacy fix, header hover underscore

--- src/js/privacy-policy.js
+++ src/js/privacy-policy.js
@@ -1,12 +1,4 @@
-const privacyPolicyLink = document.querySelector('.footer-privacy-policy');
-const policyText = document.querySelector('.policy-text');
-policyText.style.visibility = 'hidden'
-
-function showPrivacyPolicy(event) {
-    event.preventDefault();
-   
-    policyText.style.visibility = 'visible'
-   
-}
-
-privacyPolicyLink.addEventListener('click', showPrivacyPolicy);
+import "./button-up";
+import "./themeBackground";
+import "./mobileModal";
+import "./subscribeFormFooter";

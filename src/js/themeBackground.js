250a6e64c3b8c5f3576a18b7738cd283ede95bcc
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-17 22:05:57 +0200

    darkmode-button-fix

--- src/js/themeBackground.js
+++ src/js/themeBackground.js
@@ -1,57 +1,47 @@
-const themeToggle = document.querySelector('.change_theme--btn');
-const sunIcon = document.querySelector('.icon_sun');
-const moonIcon = document.querySelector('.icon_moon');
-const savedTheme = localStorage.getItem('theme');
-const body = document.querySelector('body');
-const mobileMenuBtn = document.querySelector('.mobile_menu--btn');
-const mobileMenu = document.querySelector('.mobile_menu');
+const themeToggle = document.querySelector(".change_theme--btn");
+const sunIcon = document.querySelector(".icon_sun");
+const moonIcon = document.querySelector(".icon_moon");
+const savedTheme = localStorage.getItem("theme");
+const body = document.querySelector("body");
+const mobileMenuBtn = document.querySelector(".mobile_menu--btn");
+const mobileMenu = document.querySelector(".mobile_menu");
 
-themeToggle.addEventListener('click', toggleTheme);
-mobileMenuBtn.addEventListener('click', toggleMobileMenu);
+themeToggle.addEventListener("click", toggleTheme);
+mobileMenuBtn.addEventListener("click", toggleTheme);
 
 function toggleTheme() {
   const body = document.body;
-  body.classList.toggle('dark_mode');
-  themeToggle.classList.toggle('change_theme--btn-on');
+  body.classList.toggle("dark_mode");
+  themeToggle.classList.toggle("change_theme--btn-on");
+  mobileMenuBtn.classList.toggle("show");
 
-  if (body.classList.contains('dark_mode')) {
-    localStorage.setItem('theme', 'dark');
-    sunIcon.style.display = 'none';
-    moonIcon.style.display = 'block';
+  if (body.classList.contains("dark_mode")) {
+    localStorage.setItem("theme", "dark");
+    sunIcon.style.display = "none";
+    moonIcon.style.display = "block";
   } else {
-    localStorage.setItem('theme', 'light');
-    sunIcon.style.display = 'block';
-    moonIcon.style.display = 'none';
+    localStorage.setItem("theme", "light");
+    sunIcon.style.display = "block";
+    moonIcon.style.display = "none";
   }
 }
 
-if (savedTheme === 'dark') {
-  body.classList.add('dark_mode');
-  sunIcon.style.display = 'none';
-  moonIcon.style.display = 'block';
-  themeToggle.classList.add('change_theme--btn-on');
+if (savedTheme === "dark") {
+  body.classList.add("dark_mode");
+  sunIcon.style.display = "none";
+  moonIcon.style.display = "block";
+  themeToggle.classList.add("change_theme--btn-on");
 } else {
-  body.classList.remove('dark_mode');
-  sunIcon.style.display = 'block';
-  moonIcon.style.display = 'none';
-  themeToggle.classList.remove('change_theme--btn-on');
+  body.classList.remove("dark_mode");
+  sunIcon.style.display = "block";
+  moonIcon.style.display = "none";
+  themeToggle.classList.remove("change_theme--btn-on");
 }
 
-function toggleMobileMenu() {
-  const body = document.body;
-  body.classList.toggle('dark_mode');
-  mobileMenuBtn.classList.toggle('show');
-  if (body.classList.contains('dark_mode')) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
-}
-
-if (savedTheme === 'dark') {
-  body.classList.add('dark_mode');
-  mobileMenuBtn.classList.add('show');
+if (savedTheme === "dark") {
+  body.classList.add("dark_mode");
+  mobileMenuBtn.classList.add("show");
 } else {
-  body.classList.remove('dark_mode');
-  mobileMenuBtn.classList.remove('show');
+  body.classList.remove("dark_mode");
+  mobileMenuBtn.classList.remove("show");
 }

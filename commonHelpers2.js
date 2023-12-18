672f6445537ae2ac8bb0211daabd7b9c427b8101
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-12-18 11:15:34 +0000

    Deploying to gh-pages from @ yaroslav2727/your-energy-team-project@1a40381bf449c3c98054aa8c95ea112278ec6529 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,4 +1,4 @@
-import{l as a,c as q,D as E,d as I,e as B,f as j}from"./assets/mobileModal-d693e3f0.js";import{l as D,i as g}from"./assets/vendor-c2f48a40.js";import{i as x,P as $}from"./assets/quote-06ac01e2.js";import"./assets/subscribeFormFooter-3b756eb5.js";const O="/your-energy-team-project/assets/no-image-fc78954b.jpg";function P(t){return t.map(({imgURL:e,filter:s,name:i})=>`<li class="card-exercises" data-body-exercise='${i}'>
+import{l as a,c as q,D as E,d as I,e as B,f as j}from"./assets/mobileModal-131f2f6b.js";import{l as D,i as g}from"./assets/vendor-c2f48a40.js";import{i as x,P as $}from"./assets/quote-d0b1cdf5.js";import"./assets/subscribeFormFooter-918d3d42.js";const O="/your-energy-team-project/assets/no-image-fc78954b.jpg";function P(t){return t.map(({imgURL:e,filter:s,name:i})=>`<li class="card-exercises" data-body-exercise='${i}'>
                <button type="button" class="card-exercises-img"
                  style=" background: linear-gradient(0deg, rgba(17, 17, 17, 0.50) 0%, rgba(17, 17, 17, 0.50) 100%),
                  url('${N(e)}');
